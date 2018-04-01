23c7f4dc201b945561e464c2f29cf815af3f1704
gh603 <gehui603@gmail.com>
2018-04-01 00:48:13 -0500

    Add Side Bar

---
+++ frontend/index.js
@@ -0,0 +1,9 @@
+function openSideNav(){
+    document.getElementById('mySideNav').style.width = "250px"; 
+    document.getElementById('main').style.marginLeft = "250px"; 
+}
+
+function closeSideNav(){
+    document.getElementById('mySideNav').style.width = "0"; 
+    document.getElementById('main').style.marginLeft = "0"; 
+}
