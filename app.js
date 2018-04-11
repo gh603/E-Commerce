c99898fda50ad9a13dd23465836f7290b566fef5
Zhangji Luo <luozj14@gmail.com>
2018-04-10 19:20:46 -0500

    latest

--- app.js
+++ app.js
@@ -65,6 +65,23 @@ app.post("/login", passport.authenticate("local",
     }), function(req, res){
 });
 
+app.get("/register", function(req, res){
+   res.render("register"); 
+});
+
+
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/campgrounds"); 
+        });
+    });
+});
 //================
 // Show products
 //================
