ff67a134fa0e7d9db5d68bc6c55187ef636a3c2e
wangyishi <wangyishistl@gmail.com>
2018-04-11 15:39:36 -0500

    Add files via upload
    manage page added

---
+++ E-Commerce-master/frontend/js/index.js
@@ -0,0 +1,12 @@
+function openSideNav() {
+    document.getElementById('mySideNav').style.width = "200px";
+    document.getElementById('main').style.marginLeft = "200px";
+}
+
+function closeSideNav() {
+    document.getElementById('mySideNav').style.width = "0";
+    document.getElementById('main').style.marginLeft = "0";
+}
+
+
+
