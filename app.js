7b1b0038f81e55c9dc9740031754d4366310125a
Xin Yang <yangxin0@gmail.com>
2018-04-23 21:28:36 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ var express = require("express"),
     LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
     filter = require('lodash.filter'),
-    seedDB = require("./seed"),
-    Product = require("./models/product"),
+    seedDB      = require("./seed"),
+    Product  = require("./models/product"),
     User = require("./models/user"),
     Cart = require("./models/cart"),
     Order = require("./models/order")
-
+ 
 mongoose.connect("mongodb://localhost/e-commerce");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs"); 
 app.use(express.static(__dirname));
 // app.use(express.static(__dirname + '/views'));
 
@@ -35,14 +35,14 @@ app.use(require("express-session")({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy({ usernameField: 'email', passwordField: 'password', }, User.authenticate()));
+passport.use(new LocalStrategy({usernameField: 'email', passwordField : 'password',},User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
-app.use(function (req, res, next) {
-    res.locals.currentUser = req.user;
-    next();
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
 });
 
 
@@ -50,7 +50,7 @@ app.use(function (req, res, next) {
 // Show
 //=================
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res){
     res.render("auth");
 });
 
@@ -58,105 +58,148 @@ app.get("/", function (req, res) {
 //=================
 // Authentication
 //=================
-app.get("/login", function (req, res) {
+app.get("/login", function(req, res){
     res.render("auth");
 });
 
-app.post("/login", passport.authenticate("local",
+app.post("/login", passport.authenticate("local", 
     {
-
+        
         successRedirect: "/items",
         failureRedirect: "/login"
-    }), function (req, res) {
-    });
+    }), function(req, res){
+});
 
 
 
 
-app.post("/signup", function (req, res) {
+app.post("/signup", function(req, res){
     // var newUser = new User({username: req.body.Email, FirstName: req.body.FirstName, LastName: req.body.LastName, Email: req.body.Email });
-    var newUser = new User({ username: req.body.email, fname: req.body.fname, lname: req.body.lname, email: req.body.email, isManager: true });
-    User.register(newUser, req.body.password, function (err, user) {
-        if (err) {
-
+    var newUser = new User({username: req.body.email, fname: req.body.fname, lname: req.body.lname, email: req.body.email, isManager: true});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            
             console.log(err);
             return res.render("auth");
         }
-        passport.authenticate("local")(req, res, function () {
-
-            console.log(newUser);
-            var newCart = { userId: req.user._id }
-            Cart.create(newCart, function (err, newlyCreated) {
-                if (err) {
-                    console.log(err);
+        passport.authenticate("local")(req, res, function(){
+         
+           console.log(newUser);
+           var newCart = {userId : req.user._id}
+           Cart.create(newCart, function(err, newlyCreated){
+                if(err){
+                  console.log(err);
                 } else {
-                    console.log(newlyCreated);
+                  console.log(newlyCreated);
                 }
-            });
-            res.redirect("/items");
+           });
+           res.redirect("/items"); 
         });
     });
 });
 
-app.get("/logout", function (req, res) {
-    req.logout();
-    res.redirect("/items");
+app.get("/logout", function(req, res){
+   req.logout();
+   res.redirect("/items");
 });
 //================
 // Manager
 //================
 
-app.get("/manage", function (req, res) {
+app.get("/manage", function(req, res){
     res.render("manage");
 });
 
-app.get("/login", function (req, res) {
+app.get("/login", function(req, res){
     res.render("auth");
-});
-//================
-// Show products
-//================
-app.get("/items", function (req, res) {
-    Product.find({}, function (err, allProducts) {
-        if (err) {
-            console.log(err);
-        } else {
-            //    console.log(allProducts);
-            res.render("index", { products: allProducts });
-        }
-    });
 });
 
 //================
-// Show filtered product detail
+// Show products with filter function
 //================
-app.get("/items", function (req, res) {
-    //find all products
-    Product.find({}, function (err, allProducts) {
-        if (err) {
-            console.log(err);
-        } else {
-            //filter
-            var filterProduct = lodash.filter(allProducts, x => x.category === 'Headphone');
-            res.render("index", { filterproducts: filterProduct });
-        }
-    });
+app.get("/items",function(req, res){
+  var filtercategory = '';
+  var perPage = 9;
+  var page = req.query.page || 1;
+
+  if (filtercategory === ''){
+            Product
+                    .find({})
+                    .skip((perPage * page) - perPage)
+                    .limit(perPage)
+                    .exec(function(err, allProducts){
+                        if(err){
+                            console.log(err);
+                        } else {
+                            Product.count().exec(function(err, count) {
+                                if(err){
+                                    console.log(err);
+                                } else {
+                                    res.render("index",{products:allProducts, current: page, pages: Math.ceil(count / perPage)});
+                                }
+                            });
+                        }
+                    });
+  }
+
+  else {
+            Product
+                      .find({})
+                      .skip((perPage * page) - perPage)
+                      .limit(perPage)
+                      .exec(function(err, allProducts){
+                          if(err){
+                              console.log(err);
+                          } else {
+                              var filterProduct = filter(allProducts, x => x.category === filtercategory);
+                              res.render("index",{products:filterProduct, current: page, pages: Math.ceil(filterProduct.length / perPage)});
+
+                          }
+                      });
+  }
+
 });
+
+////================
+//// Paging
+////================
+//app.get("/items", function(req, res){
+//    var perPage = 9;
+//    var page = req.query.page || 1;
+////    var pages = 3;
+//
+//    Product
+//        .find({})
+//        .skip((perPage * page) - perPage)
+//        .limit(perPage)
+//        .exec(function(err, allProducts){
+//            if(err){
+//                console.log(err);
+//            } else {
+//                Product.count().exec(function(err, count) {
+//                    if(err){
+//                        console.log(err);
+//                    } else {
+//                        res.render("index",{products:allProducts, current: page, pages: Math.ceil(count / perPage)});
+//                    }
+//                });
+//            }
+//        });
+//});
 
 //================
 // Order
 //================
 
-app.get("/orders", isLoggedIn, function (req, res) {
-    console.log('GET: orders');
-    Order.find({ userId: req.user._id }, function (err, allOrders) {
-        // Order.find({ userId: 'gehui603@gmail.com' }, function (err, allOrders) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(allOrders);
-            res.render("orders", { orders: allOrders });
-        }
+app.get("/orders", isLoggedIn, function(req, res){
+    // Order.find({_id : req.user._id}, function(err, allOrders){
+    Order.find({userId : 'gehui603@gmail.com'}, function(err, allOrders){
+       if(err){
+           console.log(err);
+       } else {
+          console.log(allOrders); 
+          res.render("orders",{orders:allOrders});
+       }
     });
 });
 
@@ -164,21 +207,20 @@ app.get("/orders", isLoggedIn, function (req, res) {
 // Checkout
 //================
 
-app.post("/orders", isLoggedIn, function (req, res) {
-    console.log('POST: orders');
-    Cart.find({ userId: req.user._id }, function (err, foundCart) {
-        if (err) {
-            console.log(err);
-        } else {
-            var newOrder = { userId: req.user._id, date: new Date(), items: foundCart.items, total: foundCart.total };
-            Order.create(newOrder, function (err, newlyCreated) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    // console.log(newlyCreated);
-                    redirect("/items");
-                }
-            });
+app.post("/orders",isLoggedIn, function(req, res){
+  Cart.find({userId : req.user._id}, function(err, foundCart){
+       if(err){
+           console.log(err);
+       } else {
+          var newOrder = {userId: req.user._id, date: new Date(), items: foundCart.items, total: foundCart.total};
+          Order.create(newOrder, function(err, newlyCreated){
+            if(err){
+               console.log(err);
+            } else {
+                // console.log(newlyCreated);
+                redirect("/orders");
+            }
+          });          
         }
     });
 });
@@ -186,58 +228,52 @@ app.post("/orders", isLoggedIn, function (req, res) {
 // Cart
 //================
 
-app.get("/cart", isLoggedIn, function (req, res) {
-    console.log("GET: cart"); 
-    Cart.find({ userId: req.user._id }).populate("items").exec(function (err, foundCart) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(foundCart);
-            res.render("cart", { carts: foundCart[0] });
-        }
+app.get("/cart", isLoggedIn, function(req, res){
+//   Cart.find({userId : req.user._id}).populate("items").exec(function(err, foundCart){
+  Cart.find({userId : "gehui603@gmail.com"}).populate("items").exec(function(err, foundCart){
+       if(err){
+           console.log(err);
+       } else {
+          console.log(foundCart);
+          res.render("cart",{carts:foundCart});
+       }
     });
 });
 
-app.post("/cart/:id", isLoggedIn, function (req, res) {
-    console.log("POST: cart");
-    Cart.find({ userId: req.user._id }, function (err, foundCart) {
-        if (err) {
-            console.log(err);
-        } else {
-            Product.findById(req.params.id, function (err, foundProduct) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(foundProduct);
-                    var item = { quantity: req.body.quantity, itemId: req.params.id };
-                    foundCart.items.push(item);
-                    redirect("/cart");
-                }
-            });
+app.post("/cart/:id", isLoggedIn ,function(req, res){
+  Cart.find({userId : req.user._id}, function(err, foundCart){
+       if(err){
+           console.log(err);
+       } else {
+            Product.findById(req.params.id, function(err, foundProduct){
+            if(err){
+              console.log(err);
+            } else {
+              console.log(foundProduct);
+              var item = {quantity: req.body.quantity, itemId: req.params.id};
+              foundCart.items.push(item);
+              redirect("/cart");
+              }
+          });
         }
     });
 });
-
-app.delete("/cart/:id", isLoggedIn, function(req, res){
-    console.log("DELETE: cart"); 
-})
 
 //================
 // Search
 //================
-app.post("/search", function (req, res) {
+app.post("/search", function(req, res){
     // Get all items from DB
-    console.log("POST: search"); 
     var keyWord = req.body.itemName;
-    console.log(keyWord);
-    Product.find({ item: keyWord }, function (err, allProducts) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(allProducts);
-            //   res.send({products: allProducts}); 
-            res.render("index", { products: allProducts });
-        }
+    console.log(keyWord); 
+    Product.find({item: keyWord}, function(err, allProducts){
+       if(err){
+           console.log(err);
+       } else {
+          console.log(allProducts);
+        //   res.send({products: allProducts}); 
+          res.render("index",{products:allProducts});
+       }
     });
 });
 
@@ -248,7 +284,7 @@ app.post("/search", function (req, res) {
 
 
 //CREATE - add new item to DB
-app.post("/items", isAdmin, function (req, res) {
+app.post("/items", isAdmin, function(req, res){
     // get data from form and add to products array
     var name = req.body.item;
     var category = req.body.category;
@@ -257,13 +293,11 @@ app.post("/items", isAdmin, function (req, res) {
     var desc = req.body.description;
     var price = req.body.price;
 
-    var newProduct = {
-        item: name, category: category, quantity: quantity,
-        image: image, description: desc, price: price
-    }
+    var newProduct = {item: name, category: category, quantity: quantity, 
+      image: image, description: desc, price: price}
     // Create a new campground and save to DB
-    Product.create(newProduct, function (err, newlyCreated) {
-        if (err) {
+    Product.create(newProduct, function(err, newlyCreated){
+        if(err){
             console.log(err);
         } else {
             console.log(newlyCreated);
@@ -274,15 +308,15 @@ app.post("/items", isAdmin, function (req, res) {
 //================
 // Show certain product detail
 //================
-app.get("/items/:id", function (req, res) {
+app.get("/items/:id", function(req, res){
     //find the product with provided ID
-    Product.findById(req.params.id, function (err, foundProduct) {
-        if (err) {
+    Product.findById(req.params.id, function(err, foundProduct){
+        if(err){
             console.log(err);
         } else {
             console.log(foundProduct)
 
-            res.render("products", { products: foundProduct });
+            res.render("products", {products: foundProduct});
         }
     });
 });
@@ -290,49 +324,49 @@ app.get("/items/:id", function (req, res) {
 // Update
 //================
 
-app.put("/:id", isAdmin, function (req, res) {
+app.put("/:id", isAdmin, function(req, res){
     // find and update the correct campground
-    Product.findByIdAndUpdate(req.params.id, req.body.product, function (err, updatedProduct) {
-        if (err) {
-            res.redirect("/items");
-        } else {
-            //redirect somewhere(show page)
-            res.redirect("/campgrounds/" + req.params.id);
-        }
+    Product.findByIdAndUpdate(req.params.id, req.body.product, function(err, updatedProduct){
+       if(err){
+           res.redirect("/items");
+       } else {
+           //redirect somewhere(show page)
+           res.redirect("/campgrounds/" + req.params.id);
+       }
     });
 });
 //================
 // Delete
 //================
-app.delete("/:id", isAdmin, function (req, res) {
+app.delete("/:id", isAdmin, function(req, res){
     //findByIdAndRemove
-    Product.findByIdAndRemove(req.params.id, function (err, foundProduct) {
-        if (err) {
+    Product.findByIdAndRemove(req.params.id, function(err, foundProduct){
+       if(err){
             res.redirect("back");
-        } else {
+       } else {
             // foundProduct.
             res.redirect("items");
-        }
+       }
     });
 });
 
-function isLoggedIn(req, res, next) {
-    if (req.isAuthenticated()) {
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
         return next();
     }
     res.redirect("/login");
 }
 
-function isAdmin(req, res, next) {
-
-    if (req.isAuthenticated() && req.user.isManager == true) {
+function isAdmin(req,res,next){
+  
+    if(req.isAuthenticated() && req.user.isManager == true){
         return next();
     }
     res.redirect("/items");
 }
 
 
-app.listen(8080, function () {
-    console.log("The E-commerce Server Has Started!");
+app.listen(8080, function(){
+   console.log("The E-commerce Server Has Started!");
 });
 
